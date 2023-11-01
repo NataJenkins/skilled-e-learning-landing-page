5a23626bacbc4f5eb4524703f06d3d7b45c79591
Natalia Macias <natajenkins@hotmail.com>
2023-11-01 15:55:05 -0600

    Header Desktop view

--- src/components/MainView/MainView.jsx
+++ src/components/MainView/MainView.jsx
@@ -1,6 +1,7 @@
 import "./MainView.scss";
 import Button from "../Button/Button";
 import DarkLogo from "../../assets/logo-dark.svg";
+import HeroDesktop from "../../assets/image-hero-desktop@2x.webp";
 export default function MainView() {
     return (
         <div className="main-container">
@@ -9,6 +10,26 @@ export default function MainView() {
                     <img src={DarkLogo} alt="" />
                     <Button text="Get Started" />
                 </nav>
+                <div className="header">
+                    <div className="details">
+                        <h1 className="heading-xl">
+                            Maximize skill, minimize budget
+                        </h1>
+                        <p className="p-m">
+                            Our modern courses across a range of in-demand
+                            skills will give you the knowledge you need to live
+                            the life you want.
+                        </p>
+                        <Button text="Get Started" secondary />
+                    </div>
+                    <div className="image">
+                        <img
+                            src={HeroDesktop}
+                            alt=""
+                            className="image-desktop"
+                        />
+                    </div>
+                </div>
             </header>
         </div>
     );
